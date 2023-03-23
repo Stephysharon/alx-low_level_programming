eb476cf637d799d685802de48c39886114ca0681
Your Name <you@example.com>
2023-03-22 19:34:00 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
  * main - Entry point
- * Description: 'prints  _putchar to stdout'
+ * Description: 'prints _putchar to stdout'
  * Return: Always 0 (success)
  */
 int main(void)
@@ -16,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
 	return (0);
 }
