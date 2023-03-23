d4308a1c376ca4e210798bbeea027946b851f418
Your Name <you@example.com>
2023-03-22 19:50:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  print_alphabet - a function that prints the alphabet, in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+		putchar('\n');
+}
+
