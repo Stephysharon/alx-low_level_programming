0d1271e26ee3e8b33aa893307e6bc83819b3ccf7
Your Name <you@example.com>
2023-03-26 03:45:25 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,24 +6,41 @@
  */
 int main(void)
 {
-	unsigned long int t0 = 0, t1 = 1, s;
+	int count;
+	unsigned long i, j, k;
+	unsigned long m, n, p, carry;
 
-        int i;
-
-        for (i = 1; i<= 98; i++)
-        {
-                s = t0 + t1;
-
-                if (i != 98)
-                {
-                        printf("%lu, ", s);
-                }
-                else
-                {
-                        printf("%lu\n", s);
-                }
-                t0 = t1;
-                t1 = s;
-        }
-        return (0);
+	count = 0;
+	i = 0;
+	j = 1;
+	for (count = 1; count <= 91; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu, ", k);
+	}
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
+	{
+		carry = (m + n) / 1000;
+		p = (m + n) - carry * 1000;
+		k = (i + j) + carry;
+		m = n;
+		n = p;
+		i = j;
+		j = k;
+		if (p >= 100)
+			printf("%lu%lu", k, p);
+		else
+			printf("%lu0%lu", k, p);
+		if (count != 98)
+			printf(", ");
+		count++;
+	}
+	putchar('\n');
+	return (0);
 }
