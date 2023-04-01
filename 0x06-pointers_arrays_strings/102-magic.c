a4c077967b382537694f58e59a88619d08e71ef8
Stephysharon <sharonchepkurui27@gmail.com>
2023-03-31 20:09:32 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,8 +8,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/**
-	 * main - a[2] = 98, followed by a new line.
+	/*
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
